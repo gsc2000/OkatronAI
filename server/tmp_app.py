f54ef2dc59b0698bb99dd0f122a301536b502d4d
Kan SAWAI <k.sawai1027@gmail.com>
2023-02-17 23:29:40 +0900

    単体処理用のPyファイル追加

---
+++ server/tmp_app.py
@@ -0,0 +1,8 @@
+from App import main_app
+
+okatron = main_app()
+async def gen(okatron: main_app):
+    while True:
+        # if start_processing:
+        # try:
+        frame = okatron.server.run()
