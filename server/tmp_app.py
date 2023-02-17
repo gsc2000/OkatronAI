ae915696018a33b42f92e4177d9e7da2af44b086
Kan SAWAI <k.sawai1027@gmail.com>
2023-02-17 23:33:03 +0900

    単体テスト用の処理変更

--- server/tmp_app.py
+++ server/tmp_app.py
@@ -1,8 +1,14 @@
 from App import main_app
+from OkatronState import Status
 
 okatron = main_app()
-async def gen(okatron: main_app):
+
+def main():
     while True:
         # if start_processing:
         # try:
+        okatron.server.state.status = Status.WORKING
         frame = okatron.server.run()
+
+if __name__ == "__main__":
+    main()
