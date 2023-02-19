eb4c7592c8b0fcb9c1feff1becba98d5bec80c11
Kan SAWAI <k.sawai1027@gmail.com>
2023-02-19 22:01:10 +0900

    GUI変更・requirements修正

--- FastAPI/App.py
+++ FastAPI/App.py
@@ -1,10 +1,15 @@
+import os
+
 from fastapi import FastAPI
 from routers import controllers
+from fastapi.staticfiles import StaticFiles
 
 import uvicorn
 
 app = FastAPI(title='Okatron AI')
 app.include_router(controllers.router)
+current = str(os.path.dirname(os.path.abspath(__file__)))
+app.mount("/", StaticFiles(directory=current + "/web", html=True), name="web")
 
 if __name__ == '__main__':
     # コンソールで [$ uvicorn run:app --reload]でも可
