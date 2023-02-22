3e68f70b8554931a9c3f12f0eba2fd59e618970d
Kan SAWAI <k.sawai1027@gmail.com>
2023-02-22 23:50:30 +0900

    ラズパイでテストするため、Webカメラに切り替え

--- server/OkatronState.py
+++ server/OkatronState.py
@@ -4,8 +4,8 @@ import enum
 
 from UserIO import UserIO
 import DataBaseapi as db
-# from Captor.WebCamera import WebCamera
-from Captor.WebCamera import NullCamera as WebCamera
+from Captor.WebCamera import WebCamera
+# from Captor.WebCamera import NullCamera as WebCamera
 from Inferencer.YOLOv5Detector import YOLOv5Detector
 from MotorController.OkatronController import OkatronController
 
