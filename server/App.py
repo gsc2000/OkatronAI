f9e8442ff7cf309f1fa045653c8f575bbba22d36
Kan SAWAI <k.sawai1027@gmail.com>
2023-02-22 22:39:56 +0900

    ・マニュアルモード、プログラムモードの画面追加 ・ボタン処理を動的に変更 　→ 画面更新がなくなる

--- server/App.py
+++ server/App.py
@@ -11,7 +11,6 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.requests import Request
-import FastapiControllers
 
 import uvicorn
 
@@ -36,8 +35,6 @@ def myArgParser() -> argparse.Namespace:
     args = parser.parse_args()
     return args
 
-
-
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse('index.html',
@@ -48,52 +45,13 @@ async def toggle_button(request: Request, button_id: int):
     if button_id == 1:
         state.mode = Mode.AUTO
         return templates.TemplateResponse("auto.html",
-                                          {"request": request,
-                                           "btn1_mode_active": True})
-    elif button_id == 2:
-        return templates.TemplateResponse("index.html",
                                           {"request": request})
-
-@app.get("/auto/ai/{button_id}")
-async def toggle_button(request: Request, button_id: int):
-    button1_active = False
-    button2_active = False
-    if button_id == 1:
-        button1_active = True
-        state.status = Status.WORKING
     elif button_id == 2:
-        button2_active = True
-        state.status = Status.IDLE
-    return templates.TemplateResponse("auto.html",
-                                      {"request": request,
-                                       "btn1_ai_active": button1_active,
-                                       "btn2_ai_active": button2_active})
-@app.get("/auto/class/{button_id}")
-async def toggle_button(request: Request, button_id: int):
-    button1_active = False
-    button2_active = False
-    button3_active = False
-    if button_id == 1:
-        button1_active = True
-        state.yolo_info["det_class"] = 0
-    elif button_id == 2:
-        button2_active = True
-        state.yolo_info["det_class"] = 16
+        return templates.TemplateResponse("manual.html",
+                                          {"request": request})
     elif button_id == 3:
-        button3_active = True
-        state.yolo_info["det_class"] = 67
-    state.resetInferencerInfo()
-    return templates.TemplateResponse("auto.html",
-                                      {"request": request,
-                                       "btn1_class_active": button1_active,
-                                       "btn2_class_active": button2_active,
-                                       "btn3_class_active": button3_active})
-
-# @app.get("/auto/iou")
-# async def iou(request: Request):
-#     iou = request.form['iou']  # テキストボックスから取得した値を変数に格納
-#     state.yolo_info["iou"] = iou
-#     state.resetInferencerInfo()
+        return templates.TemplateResponse("program.html",
+                                          {"request": request})
 
 async def gen():
     while True:
@@ -108,6 +66,71 @@ async def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return  StreamingResponse(gen(),
                     media_type='multipart/x-mixed-replace; boundary=frame')
+
+@app.post("/mode/1/ai_start")
+async def ai_start():
+    state.status = Status.WORKING
+
+@app.post("/mode/1/ai_stop")
+async def ai_stop():
+    state.status = Status.IDLE
+
+@app.post("/mode/1/class1")
+async def class1():
+    state.yolo_info["det_class"] = 0
+    state.resetInferencerInfo()
+
+@app.post("/mode/1/class2")
+async def class2():
+    state.yolo_info["det_class"] = 16
+    state.resetInferencerInfo()
+
+@app.post("/mode/1/class3")
+async def class3():
+    state.yolo_info["det_class"] = 67
+    state.resetInferencerInfo()
+
+
+# @app.get("/auto/ai/{button_id}")
+# async def toggle_button(request: Request, button_id: int):
+#     button1_active = False
+#     button2_active = False
+#     if button_id == 1:
+#         button1_active = True
+#         state.status = Status.WORKING
+#     elif button_id == 2:
+#         button2_active = True
+#         state.status = Status.IDLE
+#     return templates.TemplateResponse("auto.html",
+#                                       {"request": request,
+#                                        "btn1_ai_active": button1_active,
+#                                        "btn2_ai_active": button2_active})
+# @app.get("/auto/class/{button_id}")
+# async def toggle_button(request: Request, button_id: int):
+#     button1_active = False
+#     button2_active = False
+#     button3_active = False
+#     if button_id == 1:
+#         button1_active = True
+#         state.yolo_info["det_class"] = 0
+#     elif button_id == 2:
+#         button2_active = True
+#         state.yolo_info["det_class"] = 16
+#     elif button_id == 3:
+#         button3_active = True
+#         state.yolo_info["det_class"] = 67
+#     state.resetInferencerInfo()
+#     return templates.TemplateResponse("auto.html",
+#                                       {"request": request,
+#                                        "btn1_class_active": button1_active,
+#                                        "btn2_class_active": button2_active,
+#                                        "btn3_class_active": button3_active})
+
+# @app.get("/auto/iou")
+# async def iou(request: Request):
+#     iou = request.form['iou']  # テキストボックスから取得した値を変数に格納
+#     state.yolo_info["iou"] = iou
+#     state.resetInferencerInfo()
 
 # @app.websocket("/ws/req") # 画像とユーザリクエスト
 # async def websocket_endpoint(websocket: WebSocket):
