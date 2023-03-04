2c58a7e06e88a8017dea106b7bd63882927f9421
Kan SAWAI <k.sawai1027@gmail.com>
2023-03-05 01:24:59 +0900

    マニュアルモードのファイル構成など変更
    ・Extendsを使用した書き方は、Jinja2に依存しており、レイアウトのデバッグが困難なため、やめた
    ・マニュアルモードのボタン動作を変更
    ・ボタンが押されている間は、ボタンの色が変わるように変更
    ・JSファイルを分離

---
+++ server/static/js/manual.js
@@ -0,0 +1,85 @@
+console.log("manual.js")
+
+class PostButton {
+    constructor(button) {
+        console.log(button)
+        this.postUrl = "/mode/2/crosskey";
+        this.button = button;
+
+        this.info = this.button.id.split("_");
+
+        this.isPressed = false;
+        this.button.addEventListener('mousedown', this.handleMouseDown.bind(this));
+        this.button.addEventListener('touchstart', this.handleTouchStart.bind(this));
+        this.button.addEventListener('mouseup', this.handleMouseUp.bind(this));
+        this.button.addEventListener('touchend', this.handleTouchEnd.bind(this));
+        this.button.addEventListener('mouseleave', this.handleLeave.bind(this));
+    }
+
+    handleMouseDown() {
+        this.isPressed = true;
+        this.button.classList.add("active");
+        let data = {kind: this.info[0],
+                    direction: this.info[1]}
+        this.postData(data);
+    }
+
+    handleMouseUp(event) {
+        this.isPressed = false;
+        this.button.classList.remove("active");
+        if (this.button.contains(event.target)) {
+            let data = {kind: this.info[0],
+                        direction: "stop"}
+            this.postData(data);
+          }
+    }
+
+    handleLeave() {
+        if (this.isPressed === true){
+            this.isPressed = false;
+            this.button.classList.remove("active");
+            let data = {kind: this.info[0],
+                        direction: "stop"}
+            this.postData(data);
+        }
+    }
+
+    handleTouchStart() {
+        this.isPressed = true;
+        this.button.classList.add("active");
+        let data = {kind: this.info[0],
+                    direction: this.info[1]}
+        this.postData(data);
+    }
+
+    handleTouchEnd() {
+        this.isPressed = false;
+        this.button.classList.remove("active");
+        let data = {kind: this.info[0],
+                    direction: "stop"}
+        this.postData(data);
+    }
+
+    postData(data) {
+        console.log("ID: "+this.button.id)
+        console.log("Type: "+this.info)
+      fetch(this.postUrl, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => {
+        console.log("Response: "+response)
+      })
+      .catch(error => {
+        console.log("POST Failure")
+      });
+    }
+  }
+
+const move_buttons = document.querySelectorAll('button[id^="move_"]');
+const camera_buttons = document.querySelectorAll('button[id^="camera_"]');
+move_buttons.forEach(button => new PostButton(button))
+camera_buttons.forEach(button => new PostButton(button))
