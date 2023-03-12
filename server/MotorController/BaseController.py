722283bab2a51b98122748622d2cbbf3c42f3936
菅原啓太 <sugawarakeita@sugawarakeitas-MacBook-Pro.local>
2023-03-12 21:17:00 +0900

    サーボを１クリック２度ずつ回転する制御に変更

--- server/MotorController/BaseController.py
+++ server/MotorController/BaseController.py
@@ -113,12 +113,16 @@ class ServoController():
         self.v.start(0.0)
         self.h.start(0.0)
         
-        degree = 0
-        dc = 2.5 + (12.0-2.5)/180*(degree+90)
-        #DutyCycle dc%
+        self.max_deg_h = 60
+        self.min_deg_h = -60
+        self.max_deg_v = 15
+        self.min_deg_v = -15
+        self.degree_h = 0
+        dc = 2.5 + (12.0-2.5)/180*(self.degree_h+90)
         self.h.ChangeDutyCycle(dc)
+        self.degree_v = 0
+        dc = 2.5 + (12.0-2.5)/180*(self.degree_v+90)
         self.v.ChangeDutyCycle(dc)
-        #最大180°回転を想定し、0.3sec以上待つ
         time.sleep(1)
         #回転終了したら一旦DutyCycle0%にする
         self.h.ChangeDutyCycle(0.0)
@@ -128,62 +132,67 @@ class ServoController():
 
     def left(self):
         """左に向く"""
-        print("CAM left")
-        degree = 60
-        dc = 2.5 + (12.0-2.5)/180*(degree+90)
-        #DutyCycle dc%
-        self.h.ChangeDutyCycle(dc)
-        #最大180°回転を想定し、0.3sec以上待つ
-        time.sleep(0.3)
-        #回転終了したら一旦DutyCycle0%にする
-        self.h.ChangeDutyCycle(0.0)
+        if self.degree_h < self.max_deg_h:
+            self.degree_h = self.degree_h + 5
+            dc = 2.5 + (12.0-2.5)/180*(self.degree_h+90)
+            #DutyCycle dc%
+            self.h.ChangeDutyCycle(dc)
+            #最大180°回転を想定し、0.3sec以上待つ
+            time.sleep(0.3)
+            #回転終了したら一旦DutyCycle0%にする
+            self.h.ChangeDutyCycle(0.0)
+            print("CAM left")
+        else:
+            print("stroke limmit(CAM left)")
         pass
 
     def right(self):
         """右に向く"""
-        print("CAM right")
-        degree = -60
-        dc = 2.5 + (12.0-2.5)/180*(degree+90)
-        #DutyCycle dc%
-        self.h.ChangeDutyCycle(dc)
-        #最大180°回転を想定し、0.3sec以上待つ
-        time.sleep(0.3)
-        #回転終了したら一旦DutyCycle0%にする
-        self.h.ChangeDutyCycle(0.0)
+        if self.degree_h > self.min_deg_h:
+            self.degree_h = self.degree_h - 5
+            dc = 2.5 + (12.0-2.5)/180*(self.degree_h+90)
+            #DutyCycle dc%
+            self.h.ChangeDutyCycle(dc)
+            #最大180°回転を想定し、0.3sec以上待つ
+            time.sleep(0.3)
+            #回転終了したら一旦DutyCycle0%にする
+            self.h.ChangeDutyCycle(0.0)
+            print("CAM right")
+        else:
+            print("stroke limmit(CAM right)")
         pass
 
     def up(self):
         """前or上に向く"""
-        print("CAM up")
-        degree = 15
-        dc = 2.5 + (12.0-2.5)/180*(degree+90)
-        #DutyCycle dc%
-        self.v.ChangeDutyCycle(dc)
-        #最大180°回転を想定し、0.3sec以上待つ
-        time.sleep(0.3)
-        #回転終了したら一旦DutyCycle0%にする
-        self.v.ChangeDutyCycle(0.0)
+        if self.degree_v < self.max_deg_v:
+            self.degree_v = self.degree_v + 2
+            dc = 2.5 + (12.0-2.5)/180*(self.degree_v+90)
+            #DutyCycle dc%
+            self.v.ChangeDutyCycle(dc)
+            #最大180°回転を想定し、0.3sec以上待つ
+            time.sleep(0.3)
+            #回転終了したら一旦DutyCycle0%にする
+            self.v.ChangeDutyCycle(0.0)
+            print("CAM up")
+        else:
+            print("stroke limmit(CAM up)")
         pass
 
     def down(self):
         """後ろor下に向く"""
-        print("CAM down")
-        degree = -15
-        dc = 2.5 + (12.0-2.5)/180*(degree+90)
-        #DutyCycle dc%
-        self.v.ChangeDutyCycle(dc)
-        #最大180°回転を想定し、0.3sec以上待つ
-        time.sleep(0.3)
-        #回転終了したら一旦DutyCycle0%にする
-        self.v.ChangeDutyCycle(0.0)
+        if self.degree_v > self.min_deg_v:
+            self.degree_v = self.degree_v - 2
+            dc = 2.5 + (12.0-2.5)/180*(self.degree_v+90)
+            #DutyCycle dc%
+            self.v.ChangeDutyCycle(dc)
+            #最大180°回転を想定し、0.3sec以上待つ
+            time.sleep(0.3)
+            #回転終了したら一旦DutyCycle0%にする
+            self.v.ChangeDutyCycle(0.0)
+            print("CAM down")
+        else:
+            print("stroke limmit(CAM down)")
         pass
-
-    def forward(self):
-        """前進する"""
-        pass
-
-    def back(self):
-        """後進する"""
 
 class NullDCController():
     """机上テスト用クラス"""
